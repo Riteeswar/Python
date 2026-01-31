ce130d31b6c6d213abce7d622f72d87e32f8e162
Riteeswar <riteeswar.c-29@scds.saiuniversity.edu.in>
2026-01-31 23:28:07 +0530

    Delete Q12.py

--- Q12.py
+++
@@ -1,6 +0,0 @@
-# 12. Write a program that takes a floating-point value as input and prints its absolute value.
-
-n = float(input("Enter a number: "))
-
-n1 = n//1
-print(f"Absolute value of {n} = {n1}")
