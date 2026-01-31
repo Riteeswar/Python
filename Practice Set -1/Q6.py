6cad0d596d89675128b06dafd6cdb927452ac0f7
Riteeswar <riteeswar.c-29@scds.saiuniversity.edu.in>
2026-01-31 23:27:40 +0530

    Create Q6.py

---
+++ Practice Set -1/Q6.py
@@ -0,0 +1,12 @@
+# 6. Write a program that takes two integers a and b as input and displays whether a < b, a = b,
+# or a > b.
+
+a = int(input("Enter an integer: "))
+b = int(input("Enter other integer: "))
+
+if(a < b):
+    print("a is less than b")
+elif(a > b):
+    print("a is greater than b")
+else:
+    print("a is equal to b")
