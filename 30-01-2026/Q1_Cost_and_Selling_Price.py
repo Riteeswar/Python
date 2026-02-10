f0504e5a5810812b86b13cafc8450d908405f75d
Riteeswar <riteeswar.c-29@scds.saiuniversity.edu.in>
2026-02-10 16:43:55 +0530

    Delete Q1_Cost_and_Selling_Price.py

--- 30-01-2026/Q1_Cost_and_Selling_Price.py
+++
@@ -1,9 +0,0 @@
-# Write a program that takes as input the cost and the selling price of an item, prints whether the sale resulted in a
-# profit or loss, and prints the profit/loss incurred.
-
-a = float(input("Enter the cost of the item : "))
-b = float(input("Enter the selling price of the item : "))
-if(a > b):
-    print(f"The item is in sale and it is a profit of {a - b}")
-else:
-    print(f"The item is not in sale and it is a loss of {b - a}")
