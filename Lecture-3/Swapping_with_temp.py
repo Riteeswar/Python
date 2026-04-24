2c023b1df8dcd2fb9fae95453f001af016260ecf
Riteeswar <riteeswar.c-29@scds.saiuniversity.edu.in>
2026-04-24 23:20:19 +0530

    Delete Swapping_with_temp.py

--- Lecture-3/Swapping_with_temp.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Enter the first integer : "))
-b = int(input("Enter the second integer : "))
-
-print(f"\n{a} is the a value and {b} is the b value before swapping")
-temp = a
-a = b
-b = temp
-print(f"{a} is the a value and {b} is the b value before swapping\n")
