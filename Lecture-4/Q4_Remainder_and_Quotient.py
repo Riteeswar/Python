c997778c065076946268851fc9bf0001b003db68
Riteeswar <riteeswar.c-29@scds.saiuniversity.edu.in>
2026-04-24 23:20:40 +0530

    Delete Q4_Remainder_and_Quotient.py

--- Lecture-4/Q4_Remainder_and_Quotient.py
+++
@@ -1,2 +0,0 @@
-# Write a program that takes as input a positive integer and pprints the quotient and remainsder when the number is
-# divided by 2. NOTE: You are not allowed to use operators / and %
