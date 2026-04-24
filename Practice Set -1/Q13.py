34681d7a043eb8c045344eb366db57af7dfced23
Riteeswar <riteeswar.c-29@scds.saiuniversity.edu.in>
2026-04-24 23:28:55 +0530

    Delete Q13.py

--- Practice Set -1/Q13.py
+++
@@ -1,8 +0,0 @@
-# 13. Write a program that takes an integer as input and checks if it is divisible by 17.
-
-n = int(input("Enter an integer: "))
-
-if(n%17 == 0):
-    print(f"{n} is divisible by 17")
-else:
-    print(f"{n} is not divisible by 17")
