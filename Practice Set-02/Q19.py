1b4554e511436f1200bbb51ed4816952b9e7ed55
Riteeswar <riteeswar.c-29@scds.saiuniversity.edu.in>
2026-04-24 23:31:30 +0530

    Create Q19.py

---
+++ Practice Set-02/Q19.py
@@ -0,0 +1,12 @@
+# 19. Worker efficiency
+
+time = float(input("Enter time taken: "))
+
+if time <= 2:
+    print("Highly efficient")
+elif time <= 3:
+    print("Improve speed")
+elif time <= 4:
+    print("Training required")
+else:
+    print("Worker must leave")
