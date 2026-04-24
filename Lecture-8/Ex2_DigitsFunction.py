4ab0400a2b39757667445229e62a25e6ce7e5a40
Riteeswar <riteeswar.c-29@scds.saiuniversity.edu.in>
2026-04-24 23:23:13 +0530

    Delete Ex2_DigitsFunction.py

--- Lecture-8/Ex2_DigitsFunction.py
+++
@@ -1,9 +0,0 @@
-def count(a):
-    count = 0
-    while a > 0:
-        a = a // 10
-        count += 1
-    print(f"Count of the digits in the number is : {count}")
-
-a = int(input("Enter an integer : "))
-count(a)
